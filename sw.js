afd8f750372870df03decacd2fa854ef1b719c14
sOuL2000s <souparnopaulreborn@gmail.com>
2025-12-20 14:05:11 +0530

    updated

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'small-ai-v2-cache-v3.0'; // Increment this version number when you make significant changes to cached assets!
+const CACHE_NAME = 'small-ai-v2-cache-v3.1'; // Increment this version number when you make significant changes to cached assets!
 const urlsToCache = [
     '/', // The root HTML page
     'index.html', // The specific HTML file
